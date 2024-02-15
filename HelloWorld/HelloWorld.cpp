43c87755a5423e496b261f367ab3ae5409006873
Dheemanth Manur <dheemanthmanur72@gmail.com>
2024-02-14 18:06:42 -0800

    Works!

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -21,6 +21,12 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include <set>
+
+#include "llvm/IR/Value.h"
+#include <map>
+#include <string>
+
 using namespace llvm;
 
 //-----------------------------------------------------------------------------
@@ -30,10 +36,139 @@ using namespace llvm;
 // everything in an anonymous namespace.
 namespace {
 
+struct Operation {
+  std::string opStr;
+
+  Operation(int left, int code, int right) {
+    this->opStr = formatv("{0} {1} {2}", left, code, right).str();
+  }
+
+  Operation(llvm::Value* left) {
+   this->opStr = formatv("{0}", left).str();
+  }
+
+  bool operator==(const Operation& other) const {
+    return opStr == other.opStr;
+  }
+
+  bool operator<(const Operation& other) const {
+    return opStr < other.opStr;
+  }
+
+  
+};
+
+class VNHashTable {
+  private:
+    std::map<Operation, int> vn_data;
+    int v;
+  public:
+    VNHashTable() : v(1) {}
+
+    int insert(Operation op) {
+      int r = v;
+      vn_data[op] = v;
+      v++;
+      return r; // vn_data[op];
+    }
+    void insert(Operation op, int n) {
+      vn_data[op] = n;
+    }
+    int getVN(Operation op) {
+      if (vn_data.count(op) == 1)
+        return vn_data[op];
+      else
+        return -1;
+    }
+    std::string getContentsString() {
+      std::string contents;
+      for (const auto& pair : vn_data) {
+        contents += "HT " + pair.first.opStr +
+                    " : " + std::to_string(pair.second) + "\n";
+    }
+    return contents;
+  }
+};
+
+bool isIn(std::set<std::string> Ops, std::string ins) {
+  auto it = Ops.find(ins);
+    if (it != Ops.end())
+      return true;
+    else
+      return false;
+}
+
 // This method implements what the pass does
 void visitor(Function &F) {
-    errs() << "(llvm-tutor) Hello from: "<< F.getName() << "\n";
-    errs() << "(llvm-tutor)   number of arguments: " << F.arg_size() << "\n";
+  //auto functionName = F.getName();
+  //auto argCount = F.arg_size();
+  std::set<std::string> arthOps = {"add", "sub", "mul", "udiv", "sdiv"};
+  std::set<std::string> lsOps = {"load", "store"};
+  VNHashTable vnht;  
+
+  for (auto& basicBlock : F) {
+    // errs() << basicBlock.getName() << "\n";
+    for (auto& ins : basicBlock) {
+      if (isIn(arthOps, ins.getOpcodeName())) {
+        bool isRed = false;
+        Value *op0 = ins.getOperand(0);
+        Value *op1 = ins.getOperand(1);
+        Operation L(op0), R(op1), T(&ins);
+        int vL = vnht.getVN(L);
+        if (vL == -1) {
+          vL = vnht.insert(L);
+          //errs() << formatv("Insert {0}->{1}", L.opStr, l ) << "\n";
+        }
+        int vR = vnht.getVN(R);          
+        if (vR == -1) {
+          vR = vnht.insert(R);
+          //errs() << formatv("Insert {0}->{1}", R.opStr, r ) << "\n";
+        }
+        Operation LopR(vL, ins.getOpcode(), vR);
+        int vLopR = vnht.getVN(LopR);
+        if (vLopR != -1) {
+          //errs() << formatv("Insert {0}->{1}", LopR.opStr, vLopR ) << "\n";
+          isRed = true;
+        }
+        else {
+          isRed = false;
+          vLopR = vnht.insert(LopR);
+          //errs() << formatv("Insert {0}->{1}", LopR.opStr, lor ) << "\n";
+          //errs() << formatv("{0}", vnht.getContentsString()) << "\n";
+        }
+        vnht.insert(T, vLopR);
+        //errs () << formatv("To:{0} Op0:{1} OpCode:{2} Op1:{3} Ins:{4}", &ins ,op0, ins.getOpcode(), op1, ins) << "\n";
+        if (isRed)  
+          errs () << formatv("{0, -40} {1} = {2} {3} {4} (redundant)", ins, vLopR, vL, ins.getOpcodeName(), vR) << "\n";
+        else
+          errs () << formatv("{0, -40} {1} = {2} {3} {4}", ins, vLopR, vL, ins.getOpcodeName(), vR) << "\n";
+
+      }
+      else if (isIn(lsOps, ins.getOpcodeName())) {
+        Value *op0 = ins.getOperand(0);
+        Operation L(op0);
+        Operation T = (ins.getNumOperands() == 1) ? Operation(&ins) : Operation(ins.getOperand(1));
+        // if (ins.getNumOperands() == 1)
+        //   auto T = Operation(&ins);
+        // else
+        //   auto T = Operation(ins.getOperand(1));
+        
+        int vL = vnht.getVN(L);
+        //errs() << formatv("{0}", vnht.getContentsString()) << "\n";
+        if (vL == -1) {
+          vL = vnht.insert(L);
+          vnht.insert(T, vL);
+          //errs() << formatv("Insert {0}->{1}", L.opStr, vL) << "\n";
+          //errs() << formatv("Insert {0}->{1}", T.opStr, vL) << "\n";
+        } else {
+          vnht.insert(T, vL);
+          //errs() << formatv("Insert {0}->{1}", T.opStr, vL) << "\n";
+        }
+        //errs () << formatv("To:{0} Op0:{1} OpCode:{2} Ins:{3}", T.opStr ,op0, ins.getOpcode(), ins) << "\n";
+        errs () << formatv("{0, -40} {1} = {2}",ins , vL , vL) << "\n";
+      }
+    }
+  }
 }
 
 // New PM implementation
--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-clang -c -emit-llvm -fno-discard-value-names -O0 demo.cpp -odemo.bc
-opt -load-pass-plugin build/lib/libHelloWorld.so -passes=hello-world -disable-output demo.bc
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <demo.cpp>"
+    exit 1
+fi
+
+DEMO_CPP="$1"
+OUTPUT_BC="${DEMO_CPP%.c}.bc"
+
+clang -c -emit-llvm -fno-discard-value-names -O0 "$DEMO_CPP" -o "$OUTPUT_BC"
+opt -load-pass-plugin build/lib/libHelloWorld.so -passes=hello-world -disable-output "$OUTPUT_BC"
